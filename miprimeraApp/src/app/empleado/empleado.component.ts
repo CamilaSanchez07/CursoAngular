d699f328d69f4f1bf319a633a17911b663e2ec6d
CamilaSanchez07 <93957366+CamilaSanchez07@users.noreply.github.com>
2022-11-19 19:52:14 -0500

    Curso Angular. Video 6-10

--- miprimeraApp/src/app/empleado/empleado.component.ts
+++ miprimeraApp/src/app/empleado/empleado.component.ts
@@ -4,9 +4,36 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
+  // template: "<p>Aquí iría un empleado</p>",
+  // styles: ["p{background-color:red;}"]
 })
 export class EmpleadoComponent implements OnInit {
 
+  nombre = "Camila";
+  apellido = "Sánchez";
+  edad = 17;
+  // empresa = "CBTis 72";
+
+  // getEdad(){
+  //   return this.edad;
+  // }
+
+  habilitacionCuadro = false;
+  userRegistrado = true;
+  textoRegistro = "No hay ningún registro";
+
+  getRegistroUsuario() {
+    this.userRegistrado = false;
+  }
+
+  setUsuarioRegistrado(event:Event) {
+    if((<HTMLInputElement>event.target).value=="si") {
+      this.textoRegistro = "Usuario registrado!"; 
+    } else {
+      this.textoRegistro = "No hay ningún registro";
+    }
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
